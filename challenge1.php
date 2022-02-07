ccec571a1fe8eb9ec6fb357418b1b67aaa4080c0
Antoine75005 <ajabruneau@orange.fr>
2022-02-07 20:36:15 +0100

    challenge1.php

---
+++ challenge1.php
@@ -0,0 +1,23 @@
+<?php
+ $booktitle= 'Voyage avec un âne dans les Cévennes';
+
+ $read = true;
+
+ $released = 2017;
+
+ $price = 5.99;
+
+ echo "$booktitle<br>";
+
+ if ($read == true) {
+     echo "Excellent livre.<br>";
+ } 
+
+ else {
+     echo "à lire.<br>";
+ }
+ 
+ echo "$released<br>";
+
+ echo "$price<br>";
+
